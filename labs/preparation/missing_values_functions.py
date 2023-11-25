76125345a7f528ac35f89c9c40b5e024c5e28908
Ådne <aadne.t.svendsrud@gmail.com>
2023-11-25 18:39:06 +0000

    feat: missing value imputation credit_score

---
+++ labs/preparation/missing_values_functions.py
@@ -0,0 +1,212 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import show
+
+from helpers.dslabs_functions import mvi_by_dropping
+
+
+def check_differing_values(df, column):
+    # Group by 'Customer_ID' and get unique values in 'column'
+    # nan is counted as a value
+    unique_values = fin.groupby('Customer_ID')[column].unique()
+
+    # Filter the 'Customer_IDs' where the array has length 2 or more, meaning that there are differing values
+    selected_customer_ids = unique_values[unique_values.apply(lambda x: len(x) > 2)].index
+
+    print(f"There are {selected_customer_ids.size} customers with differing values for {column}")
+
+
+fin = pd.read_csv("../../datasets/prepared/class_credit_score_encoded_1.csv")
+
+
+def mvi_amputation(fin):
+    # find missing values
+    mv = {}
+    for col in fin.columns:
+        num = fin[col].isna().sum()
+        if num > 0:
+            mv[col] = num
+
+    # print(mv)
+    print(fin.shape)
+
+    print(f"col\trow")
+    for i in range(101):
+        print({i / 100})
+        print(mvi_by_dropping(fin, min_pct_per_variable=0.0, min_pct_per_record=(i / 100)).shape[0])
+
+
+def count_distribution_for_missing_values_per_customer(fin, column):
+    counts = fin[fin[column].isna()]['Customer_ID'].value_counts()
+
+    # Count how many times each count value occurs
+    count_distribution = counts.value_counts().sort_index()
+
+    print(count_distribution)
+
+
+
+
+def impute_values(group, diff):
+    start = group['Credit_History_Age'].dropna().iloc[0]
+    return pd.Series(start + diff * np.arange(len(group)), index=group.index)
+
+
+def impute_credithistory(df):
+    diff = df['Credit_History_Age'][3] - df['Credit_History_Age'][2]
+    df['Credit_History_Age'] = df.groupby('Customer_ID').apply(impute_values, diff=diff).reset_index(level=0, drop=True)
+
+
+def impute_column(df, column_name, method='mean', rounding=0):
+    """
+    Impute missing values in a column by either mean, mode or median. This groups together all rows with the same
+    Customer_ID and uses that to impute the missing values.
+    :param df: DataFrame
+    :param column_name: String
+    :param method: 'mean', 'mode' or 'median'
+    :param rounding: int
+    :return: None
+    """
+    if method == 'mean':
+        mean_values = df.groupby('Customer_ID')[column_name].transform('mean')
+        mean_values = round(mean_values, rounding)
+        df[column_name] = df[column_name].fillna(mean_values)
+    elif method == 'mode':
+        mode_values = df.groupby('Customer_ID')[column_name].transform(lambda x: x.mode()[0])
+        df[column_name] = df[column_name].fillna(mode_values)
+    elif method == 'median':
+        median_values = df.groupby('Customer_ID')[column_name].transform('median')
+        median_values = round(median_values, rounding)
+        df[column_name] = df[column_name].fillna(median_values)
+    else:
+        print("Invalid method specified")
+
+
+def render_mv(df):
+    print(f"Dataset nr records={df.shape[0]}", f"nr variables={df.shape[1]}")
+
+    mv: dict[str, int] = {}
+    for var in df.columns:
+        nr: int = df[var].isna().sum()
+        if "Loan" not in var and "Not Specified" not in var and "cos" not in var and "sin" not in var:
+            mv[var] = nr
+
+    print(f"Missing values: {mv}")
+    for i in mv:
+        print(f"{i}\t{mv[i]}")
+
+    variables = list(mv.keys())
+    missing_values = list(mv.values())
+    sns.set_style("whitegrid")
+    sns.set_context("talk")
+    plt.figure(figsize=(20, 8))
+    bars = sns.barplot(x=variables, y=missing_values)
+    plt.xticks(rotation=45, ha='right')
+
+    plt.title('Number of Missing Values per Variable', fontsize=24)
+    plt.ylabel('Number of Missing Values', fontsize=18)
+
+    for bar in bars.patches:
+        bars.annotate(format(bar.get_height(), ',.0f'),  # No decimal places, include commas as thousands separators.
+                      (bar.get_x() + bar.get_width() / 2, bar.get_height()),
+                      ha='center', va='center',
+                      size=13, xytext=(0, 8),
+                      textcoords='offset points')
+
+    # sns.despine()
+    plt.tight_layout()
+    plt.savefig('../../figures/temp/missing_values_per_variable.png')
+    show()
+
+
+# Reasonings for the imputation modes finances
+if __name__ == '__main__':
+    fin = mvi_by_dropping(fin, min_pct_per_variable=0.0, min_pct_per_record=0.9)  # Drops ~ 400 rows of 100k
+
+    count_distribution_for_missing_values_per_customer(fin, "NumofDelayedPayment")
+    '''
+    Output:
+    count
+    1    4069
+    2    1113
+    3     156
+    4      15  # <--- 15 customers have 4 missing values
+    5       1  # <--- 1 customer has 5 missing values
+
+    Based on this, we can imputate the missing values for the rest of the customers, but for these 16 it will be a bit
+    misleading. Given that 16 << 12500 (0.1%), we can just take it for what it is
+    '''
+
+    count_distribution_for_missing_values_per_customer(fin, "ChangedCreditLimit")
+    '''
+    count
+    1    1790
+    2     108
+    3       8
+    No issues here, we can imputate the missing values
+    '''
+
+    count_distribution_for_missing_values_per_customer(fin, "NumCreditInquiries")
+    '''
+    count
+    1    1656
+    2     126
+    3       4
+    No issues here, we can imputate the missing values
+    '''
+
+    count_distribution_for_missing_values_per_customer(fin, "CreditMix")
+    check_differing_values(fin, "CreditMix")
+    '''
+    count
+    1    4276
+    2    3597
+    3    1865
+    4     558
+    5     110
+    6      12
+
+    Here we have a lot of missing values.
+    CreditMix is a symbolic value:
+    Bad - 0
+    Standard - 1
+    Good - 2
+
+    After running check_differing_values, we can see that there are no customers who have more than one value for CreditMix.
+    Meaning imputation by mode is fine here.
+    '''
+
+    count_distribution_for_missing_values_per_customer(fin, "Amountinvestedmonthly")
+    '''
+    count
+    1    3168
+    2     517
+    3      42
+    4       2
+    5       2
+    I hope using mean is fine here, but there are some outliers here (usually the exact amount 10 000)
+    '''
+
+    count_distribution_for_missing_values_per_customer(fin, "Payment_Behaviour")
+    '''
+    count
+    1    4403
+    2    1166
+    3     194
+    4      23
+    5       2
+
+    25 people are 0.2% of the total, so imputing here should be fine?
+    '''
+
+    count_distribution_for_missing_values_per_customer(fin, "MonthlyBalance")
+    '''
+    count
+    1    755
+    2    146
+    3     25
+    4     10
+    5      1
+    '''
--- labs/preparation/missing_values_services.py
+++ labs/preparation/missing_values_services.py
@@ -1,117 +1,7 @@
-import matplotlib.pyplot as plt
 import pandas as pd
-from matplotlib.pyplot import figure, show
-from helpers.dslabs_functions import plot_bar_chart, get_variable_types, define_grid, HEIGHT, plot_multibar_chart, \
-    determine_outlier_thresholds_for_var, count_outliers, derive_date_variables, analyse_date_granularity, \
-    mvi_by_filling, mvi_by_dropping
-import seaborn as sns
-import numpy as np
 
-
-def check_occupation_row(row, df):
-    if pd.isna(row['Occupation']):
-        # Check row below
-        if row['Customer_ID'] == df['Customer_ID'].iloc[row.name + 1]:
-            if pd.notna(df['Occupation'].iloc[row.name + 1]):
-                df['Occupation'].iloc[row.name] = df['Occupation'].iloc[row.name + 1]
-                # Propagate the value to the rows above as well
-                for i in range(1, 3):
-                    if row['Customer_ID'] == df['Customer_ID'].iloc[row.name - i]:
-                        if pd.isna(
-                                df['Occupation'].iloc[row.name - i]
-                        ):
-                            df['Occupation'].iloc[row.name - i] = df['Occupation'].iloc[row.name + 1]
-                        else:
-                            break
-
-                return
-        # Check row above
-        if row['Customer_ID'] == df['Customer_ID'].iloc[row.name - 1]:
-            if pd.notna(df['Occupation'].iloc[row.name - 1]):
-                df['Occupation'].iloc[row.name] = df['Occupation'].iloc[row.name - 1]
-                return
-
-
-def check_age_row(row, df):
-    age = row['Age']
-
-    if pd.isna(age) or age > 122:
-        # Check row below
-        if row['Customer_ID'] == df['Customer_ID'].iloc[row.name + 1]:
-            age = df['Age'].iloc[row.name + 1]
-            if pd.notna(age):
-                if age < 122:
-                    df['Age'].iloc[row.name] = age
-                    # Propagate the value to the row above as well, unless it is the same age - 1
-                    # (then the person has had a birthday)
-                    if row['Customer_ID'] == df['Customer_ID'].iloc[row.name - 1] and age != df['Age'].iloc[
-                        row.name - 1] + 1:
-                        df['Age'].iloc[row.name - 1] = age
-                    return
-
-        # Check row above
-        if row['Customer_ID'] == df['Customer_ID'].iloc[row.name - 1]:
-            age = df['Age'].iloc[row.name - 1]
-            if pd.notna(age):
-                if age < 122:
-                    df['Age'].iloc[row.name] = age
-                    return
-
-
-# We assume that people are not unemployed. !!!
-def check_monthly_inhand_salary_row(row, df):
-    monthly_inhand_salary = row['Monthly_Inhand_Salary']
-
-    if pd.isna(monthly_inhand_salary):
-        # Check row below
-        if row['Customer_ID'] == df['Customer_ID'].iloc[row.name + 1]:
-            monthly_inhand_salary = df['Monthly_Inhand_Salary'].iloc[row.name + 1]
-            if pd.notna(monthly_inhand_salary):
-                df['Monthly_Inhand_Salary'].iloc[row.name] = monthly_inhand_salary
-                # Propagate the value to the rows above as well
-                for i in range(1, 4):
-                    if row['Customer_ID'] == df['Customer_ID'].iloc[row.name - i]:
-                        if pd.isna(
-                                df['Monthly_Inhand_Salary'].iloc[row.name - i]
-                        ):
-                            df['Monthly_Inhand_Salary'].iloc[row.name - i] = monthly_inhand_salary
-                        else:
-                            break
-                return
-
-        # Check row above
-        if row['Customer_ID'] == df['Customer_ID'].iloc[row.name - 1]:
-            monthly_inhand_salary = df['Monthly_Inhand_Salary'].iloc[row.name - 1]
-            if pd.notna(monthly_inhand_salary):
-                df['Monthly_Inhand_Salary'].iloc[row.name] = monthly_inhand_salary
-                return
-
-
-def impute_column(df, column_name, method='mean', rounding=True):
-    """
-    Impute missing values in a column by either mean, mode or median. This groups together all rows with the same
-    Customer_ID and uses that to impute the missing values.
-    :param df: DataFrame
-    :param column_name: 'mean', 'mode' or 'median
-    :param method: 'mean', 'mode' or 'median
-    :param rounding: Boolean
-    :return: None
-    """
-    if method == 'mean':
-        mean_values = df.groupby('Customer_ID')[column_name].transform('mean')
-        if rounding:
-            mean_values = mean_values.round()
-        df[column_name] = df[column_name].fillna(mean_values)
-    elif method == 'mode':
-        mode_values = df.groupby('Customer_ID')[column_name].transform(lambda x: x.mode()[0])
-        df[column_name] = df[column_name].fillna(mode_values)
-    elif method == 'median':
-        median_values = df.groupby('Customer_ID')[column_name].transform('median')
-        if rounding:
-            median_values = median_values.round()
-        df[column_name] = df[column_name].fillna(median_values)
-    else:
-        print("Invalid method specified")
+from helpers.dslabs_functions import mvi_by_dropping, mvi_by_filling
+from labs.preparation.missing_values_functions import impute_credithistory, impute_column
 
 
 def imputate_missing_values_services():
@@ -119,69 +9,31 @@ def imputate_missing_values_services():
     df = pd.read_csv(filename, na_values="")
 
     # Drop 400 rows with missing values, then reset indices
+    # We should also test for dropping and not dropping records
     df = mvi_by_dropping(df, min_pct_per_variable=0.0, min_pct_per_record=0.9)
     df.reset_index(drop=True, inplace=True)
 
-    # render_mv(df)
+    # Custom imputation
     impute_column(df, 'Age', 'mode')
     impute_column(df, 'Occupation', 'mode')
     impute_column(df, 'Monthly_Inhand_Salary', 'mode')
     impute_column(df, 'NumofDelayedPayment', 'mean')
-    impute_column(df, 'ChangedCreditLimit', 'mean', rounding=False)
+    impute_column(df, 'ChangedCreditLimit', 'mean')
     impute_column(df, 'NumCreditInquiries', 'mode')
+    impute_column(df, 'CreditMix', 'mode')
+    impute_credithistory(df)
+    impute_column(df, 'Amountinvestedmonthly', 'mean', rounding=5)
+    impute_column(df, 'Payment_Behaviour', 'mode')
+    impute_column(df, 'MonthlyBalance', 'mean', rounding=5)
 
-    # render_mv(df)
-    df['NumofDelayedPayment'].describe()
-    print(df.shape)
+    df.to_csv('../../datasets/prepared/class_credit_score_2_1.csv', index=False)
 
-    # df.to_csv('../../datasets/prepared/class_credit_score_encoded_2.csv', index=False)
-
-    # KNN and frequent imputation
-    '''try:
+    # KNN imputation
+    try:
         df.drop(columns=["Customer_ID"], inplace=True)
     except KeyError:
         pass
-    knn = mvi_by_filling(df, 'knn')
-    freq = mvi_by_filling(df, 'frequent')
-    knn.to_csv('../../datasets/prepared/class_credit_score_encoded_2_MVI_knn.csv', index=False)
-    freq.to_csv('../../datasets/prepared/class_credit_score_encoded_2_MVI_freq.csv', index=False)'''
-
-
-def render_mv(df):
-    print(f"Dataset nr records={df.shape[0]}", f"nr variables={df.shape[1]}")
-
-    mv: dict[str, int] = {}
-    for var in df.columns:
-        nr: int = df[var].isna().sum()
-        if "Loan" not in var and "Not Specified" not in var and "cos" not in var and "sin" not in var:
-            mv[var] = nr
-
-    print(f"Missing values: {mv}")
-    for i in mv:
-        print(f"{i}\t{mv[i]}")
-
-    variables = list(mv.keys())
-    missing_values = list(mv.values())
-    sns.set_style("whitegrid")
-    sns.set_context("talk")
-    plt.figure(figsize=(20, 8))
-    bars = sns.barplot(x=variables, y=missing_values)
-    plt.xticks(rotation=45, ha='right')
-
-    plt.title('Number of Missing Values per Variable', fontsize=24)
-    plt.ylabel('Number of Missing Values', fontsize=18)
-
-    for bar in bars.patches:
-        bars.annotate(format(bar.get_height(), ',.0f'),  # No decimal places, include commas as thousands separators.
-                      (bar.get_x() + bar.get_width() / 2, bar.get_height()),
-                      ha='center', va='center',
-                      size=13, xytext=(0, 8),
-                      textcoords='offset points')
-
-    # sns.despine()
-    plt.tight_layout()
-    plt.savefig('../../figures/temp/missing_values_per_variable.png')
-    show()
+    mvi_by_filling(df, 'knn').to_csv('../../datasets/prepared/class_credit_score_2_knn.csv', index=False)
 
 
 if __name__ == "__main__":
