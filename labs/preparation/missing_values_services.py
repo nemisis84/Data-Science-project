a4af5f288d551406ae66fe8091e96529726fb80c
Ådne <aadne.t.svendsrud@gmail.com>
2023-11-26 14:04:13 +0000

    refactor: change function name for services imputation

--- labs/preparation/missing_values_services.py
+++ labs/preparation/missing_values_services.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from helpers.dslabs_functions import mvi_by_dropping, mvi_by_filling
-from labs.preparation.missing_values_functions import impute_credithistory, impute_column
+from labs.preparation.missing_values_functions import impute_credithistory, impute_column_finance
 
 
 def imputate_missing_values_services():
@@ -14,26 +14,27 @@ def imputate_missing_values_services():
     df.reset_index(drop=True, inplace=True)
 
     # Custom imputation
-    impute_column(df, 'Age', 'mode')
-    impute_column(df, 'Occupation', 'mode')
-    impute_column(df, 'Monthly_Inhand_Salary', 'mode')
-    impute_column(df, 'NumofDelayedPayment', 'mean')
-    impute_column(df, 'ChangedCreditLimit', 'mean')
-    impute_column(df, 'NumCreditInquiries', 'mode')
-    impute_column(df, 'CreditMix', 'mode')
+    impute_column_finance(df, 'Age', 'mode')
+    impute_column_finance(df, 'Occupation', 'mode')
+    impute_column_finance(df, 'Monthly_Inhand_Salary', 'mode')
+    impute_column_finance(df, 'NumofDelayedPayment', 'mean')
+    impute_column_finance(df, 'ChangedCreditLimit', 'mean')
+    impute_column_finance(df, 'NumCreditInquiries', 'mode')
+    impute_column_finance(df, 'CreditMix', 'mode')
     impute_credithistory(df)
-    impute_column(df, 'Amountinvestedmonthly', 'mean', rounding=5)
-    impute_column(df, 'Payment_Behaviour', 'mode')
-    impute_column(df, 'MonthlyBalance', 'mean', rounding=5)
+    impute_column_finance(df, 'Amountinvestedmonthly', 'mean', rounding=5)
+    impute_column_finance(df, 'Payment_Behaviour', 'mode')
+    impute_column_finance(df, 'MonthlyBalance', 'mean', rounding=5)
 
     try:
         df.drop(columns=["Customer_ID"], inplace=True)
     except KeyError:
         pass
-    
+
     df.to_csv('../../datasets/prepared/class_credit_score_2_1.csv', index=False)
 
     # KNN imputation
+    df = pd.read_csv(filename, na_values="")
     mvi_by_filling(df, 'knn').to_csv('../../datasets/prepared/class_credit_score_2_knn.csv', index=False)
 
 
