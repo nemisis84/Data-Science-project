619bbd6384fc1c05913bfbcf7b1ac7797eb546a2
Ådne <aadne.t.svendsrud@gmail.com>
2023-11-26 19:19:41 +0000

    chore: change functions for pipeline

--- labs/preparation/outliers_replace.py
+++ labs/preparation/outliers_replace.py
@@ -1,28 +1,21 @@
-from pandas import read_csv, DataFrame
+from pandas import DataFrame
 from helpers.dslabs_functions import (
     get_variable_types,
     determine_outlier_thresholds_for_var,
 )
 
 
-def replace_outliers(data: DataFrame):
+def replace_outliers(data: DataFrame, save=False, file_prefix=""):
     numeric_vars: list[str] = get_variable_types(data)["numeric"]
-    if [] != numeric_vars:
-        df: DataFrame = data.copy(deep=True)
-        summary5: DataFrame = data[numeric_vars].describe()
-        for var in numeric_vars:
-            top, bottom = determine_outlier_thresholds_for_var(summary5[var])
-            median: float = df[var].median()
-            df[var] = df[var].apply(lambda x: median if x > top or x < bottom else x)
-        df.to_csv("../../datasets/prepared/outliers_replaced.csv", index=False)
-        print("Data after replacing outliers:", df.shape)
-        print(df.describe())
-    else:
-        print("There are no numeric variables")
 
-
-
-
-
-
-
+    df: DataFrame = data.copy(deep=True)
+    summary5: DataFrame = data[numeric_vars].describe()
+    for var in numeric_vars:
+        top, bottom = determine_outlier_thresholds_for_var(summary5[var])
+        median: float = df[var].median()
+        df[var] = df[var].apply(lambda x: median if x > top or x < bottom else x)
+    if save:
+        df.to_csv(f"../../datasets/prepared/{file_prefix}_3_replaced.csv", index=False)
+    print("Data after replacing outliers:", df.shape)
+    print(df.describe())
+    return df
--- labs/preparation/outliers_truncate.py
+++ labs/preparation/outliers_truncate.py
@@ -1,24 +1,22 @@
-from pandas import read_csv, DataFrame
+from pandas import DataFrame
 from helpers.dslabs_functions import (
     get_variable_types,
     determine_outlier_thresholds_for_var,
 )
 
 
-def truncate_outliers(data: DataFrame):
+def truncate_outliers(data: DataFrame, save=False, file_prefix=""):
     numeric_vars: list[str] = get_variable_types(data)["numeric"]
-    if [] != numeric_vars:
-        df: DataFrame = data.copy(deep=True)
-        summary5: DataFrame = data[numeric_vars].describe()
-        for var in numeric_vars:
-            top, bottom = determine_outlier_thresholds_for_var(summary5[var])
-            df[var] = df[var].apply(
-                lambda x: top if x > top else bottom if x < bottom else x
-            )
-        df.to_csv("../../datasets/prepared/outliers_truncated.csv", index=True)
-        print("Data after truncating outliers:", df.shape)
-        print(df.describe())
-    else:
-        print("There are no numeric variables")
 
-truncate_outliers(read_csv("../../datasets/prepared/class_pos_covid_encoded_1.csv"))
+    df: DataFrame = data.copy(deep=True)
+    summary5: DataFrame = data[numeric_vars].describe()
+    for var in numeric_vars:
+        top, bottom = determine_outlier_thresholds_for_var(summary5[var])
+        df[var] = df[var].apply(
+            lambda x: top if x > top else bottom if x < bottom else x
+        )
+    if save:
+        df.to_csv(f"../../datasets/prepared/{file_prefix}_3_truncated.csv", index=True)
+    print("Data after truncating outliers:", df.shape)
+    print(df.describe())
+    return df
