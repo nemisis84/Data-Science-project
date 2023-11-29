0926dac352f54ae8b3eeebffc2b943a9d42277ee
Simen Myrrusten <simen.myrrusten@tecnico.ulisboa.pt>
2023-11-29 20:17:27 +0000

    Gradient booster implementation

--- labs/Evaluation/gradient_boosting.py
+++ labs/Evaluation/gradient_boosting.py
@@ -7,6 +7,9 @@ from helpers.dslabs_functions import (
     read_train_test_from_files,
 )
 from helpers.dslabs_functions import HEIGHT, plot_evaluation_results, plot_multiline_chart
+from numpy import std, argsort
+from helpers.dslabs_functions import plot_horizontal_bar_chart
+import matplotlib.pyplot as plt
 
 def gradient_boosting_study(
     trnX: ndarray,
@@ -18,7 +21,7 @@ def gradient_boosting_study(
     metric: str = "accuracy",
 ) -> tuple[GradientBoostingClassifier | None, dict]:
     n_estimators: list[int] = [100] + [i for i in range(500, nr_max_trees + 1, lag)]
-    max_depths: list[int] = [2, 8, 15]
+    max_depths: list[int] = [5, 8, 11]
     learning_rates: list[float] = [0.1, 0.3, 0.5, 0.7, 0.9]
 
     best_model: GradientBoostingClassifier | None = None
@@ -62,8 +65,66 @@ def gradient_boosting_study(
 
     return best_model, best_params
 
+def gradient_boosting_variables_studies(best_model, vars, path):
+    trees_importances: list[float] = []
+    for lst_trees in best_model.estimators_:
+        for tree in lst_trees:
+            trees_importances.append(tree.feature_importances_)
 
-def main(train_filename, test_filename, target, path = False, eval_metric = "accuracy"):
+    stdevs: list[float] = list(std(trees_importances, axis=0))
+    importances = best_model.feature_importances_
+    indices: list[int] = argsort(importances)[::-1]
+    elems: list[str] = []
+    imp_values: list[float] = []
+    for f in range(len(vars)):
+        elems += [vars[indices[f]]]
+        imp_values.append(importances[indices[f]])
+        print(f"{f+1}. {elems[f]} ({importances[indices[f]]})")
+
+    plt.figure(figsize=(9, 8))
+    plot_horizontal_bar_chart(
+        elems,
+        imp_values,
+        error=stdevs,
+        title="GB variables importance",
+        xlabel="importance",
+        ylabel="variables",
+        percentage=True,
+    )
+    if path:
+        plt.yticks(rotation=45)
+        savefig(path+"gradient_boosting_variables_study")
+
+def gradient_boosting_overfitting_study(trnX, trnY, eval_metric = "accuracy", path=False):
+    d_max: int = params["params"][0]
+    lr: float = params["params"][1]
+    nr_estimators: list[int] = [i for i in range(2, 2501, 500)]
+
+    y_tst_values: list[float] = []
+    y_trn_values: list[float] = []
+    acc_metric: str = "accuracy"
+
+    for n in nr_estimators:
+        clf = GradientBoostingClassifier(n_estimators=n, max_depth=d_max, learning_rate=lr)
+        clf.fit(trnX, trnY)
+        prd_tst_Y: array = clf.predict(tstX)
+        prd_trn_Y: array = clf.predict(trnX)
+        y_tst_values.append(CLASS_EVAL_METRICS[acc_metric](tstY, prd_tst_Y))
+        y_trn_values.append(CLASS_EVAL_METRICS[acc_metric](trnY, prd_trn_Y))
+
+    figure()
+    plot_multiline_chart(
+        nr_estimators,
+        {"Train": y_trn_values, "Test": y_tst_values},
+        title=f"GB overfitting study for d={d_max} and lr={lr}",
+        xlabel="nr_estimators",
+        ylabel=str(eval_metric),
+        percentage=True,
+    )
+    if path:
+        savefig(path+"gradient_boosting_overfitting_study")
+
+def study_and_train(train_filename, test_filename, target, path = False, eval_metric = "accuracy"):
     trnX, tstX, trnY, tstY, labels, vars = read_train_test_from_files(
         train_filename, test_filename, target
     )
@@ -81,30 +142,39 @@ def main(train_filename, test_filename, target, path = False, eval_metric = "acc
         metric=eval_metric,
     )
     if path:
-        savefig(path)
-    return best_model, params
-
-if __name__ == "__main__":
-    
-    # Covid dataset
-    train_filename = ""
-    test_filename = ""
-    target = "PosCovid"
-    path = "/figures/CovidPos/Evaluation/gradientBoosting_paramStudy.png"
-    best_model, params = main(train_filename, test_filename, target, path)
+        savefig(path+"gradient_boosting_study")
+    print("Training ...")
     prd_trn: array = best_model.predict(trnX)
     prd_tst: array = best_model.predict(tstX)
     figure()
     plot_evaluation_results(params, trnY, prd_trn, tstY, prd_tst, labels)
-    savefig(f'images/{file_tag}_gb_{params["name"]}_best_{params["metric"]}_eval.png')
-    show()
+    if path:
+        plt.tight_layout()
+        savefig(path+"gradient_boosting_best_results")
+    
+    return best_model, params, trnX, tstX, trnY, tstY, vars
+
+if __name__ == "__main__":
+    
+    dataset_path = "../../datasets/prepared/"
+
+    # Covid dataset
+    train_filename = dataset_path + "7_CovidPos_train.csv"
+    test_filename = dataset_path + "6_CovidPos_select_features__test_variance.csv"
+    target = "CovidPos"
+    figure_path = "../../figures/CovidPos/Evaluation/"
+    best_model, params, trnX, tstX, trnY, tstY, vars = study_and_train(train_filename, test_filename, target, path = figure_path)
+    gradient_boosting_variables_studies(best_model, vars, path=figure_path)
+    gradient_boosting_overfitting_study(trnX, trnY, path=figure_path)
 
 
     # Credit dataset
-    train_filename = ""
-    test_filename = ""
-    target = "PosCovid"
-    path = "/figures/Credit_Score /Evaluation/gradientBoosting_paramStudy.png"
-
+    train_filename = dataset_path + "7_Credit_Score_train.csv"
+    test_filename = dataset_path +"6_Credit_Score_select_features__test_variance.csv"
+    target = "Credit_Score"
+    figure_path = "../../figures/Credit_Score/Evaluation/"
+    best_model, params, trnX, tstX, trnY, tstY, vars = study_and_train(train_filename, test_filename, target, path = figure_path)
+    gradient_boosting_variables_studies(best_model, vars, path=figure_path)
+    gradient_boosting_overfitting_study(trnX, trnY, path=figure_path)
 
 
